b836222cc816806c8568cfb354f68cbd3a7d9893
Rudraprasad-Mohapatra <rudraprasadmohapatra51@gmail.com>
2023-11-19 12:16:05 +0530

    No-SQL MongoDB added to store the usr queries

--- app.py
+++ app.py
@@ -1,26 +1,62 @@
 from flask import Flask, render_template, request, jsonify
+from pymongo import MongoClient
+from pymongo.errors import ServerSelectionTimeoutError
+import datetime
 from app.chatbot.chatbot_bourntec import out
 
 app = Flask(__name__)
 
-@app.route('/ping', methods=['GET'])
+# Replace <YOUR_CONNECTION_STRING> with your MongoDB connection string
+client = MongoClient(
+    "mongodb+srv://rudraprasadmohapatra51:AJkCE0Qb9UEXbUfX@chatbot.738aw0x.mongodb.net/"
+)
+
+db = client["Chatbot"]  # Replace "your_database_name" with your database name
+
+collection = db["user_queries"]
+
+
+def log_user_query(query):
+    try:
+        # Insert user query into the MongoDB collection
+        log_entry = {"query": query, "timestamp": datetime.datetime.utcnow()}
+        collection.insert_one(log_entry)
+    except ServerSelectionTimeoutError as e:
+        print("Failed to log user query to MongoDB:", e)
+
+
+@app.route("/ping", methods=["GET"])
 def ping():
     # out("message")
-    return jsonify({'response': "pong"})
+    return jsonify({"response": "pong"})
 
-@app.route('/chat', methods=['POST'])
+
+@app.route("/")
+def index():
+    return render_template("chat_ui.html")
+
+
+@app.route("/chat", methods=["POST"])
 def chat():
     data = request.get_json()
-    query = data.get('query')
+    query = data.get("query")
+
+    # Log the user query to MongoDB
+    log_user_query(query)
+
     # Use your chatbot logic from the chatbot_logic module
 
     response = out(query)
 
-    return jsonify({'response': response})
+    return jsonify({"response": response})
 
-@app.route('/')
-def index():
-    return render_template('chat_ui.html')
 
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5005, debug=True)
+if __name__ == "__main__":
+    try:
+        client.server_info()
+        print("Connected to MongoDB")
+    except ServerSelectionTimeoutError as e:
+        print("Failed to connect to MongoDB:", e)
+
+    print("Server running at port: 5005")
+    app.run(host="127.0.0.1", port=5005, debug=True, use_reloader=True)
